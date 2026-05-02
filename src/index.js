8b1f92c9fe97358098ec01dbc13e1eb69532b53b
Million <million.mburu@student.moringaschool.com>
2026-05-02 16:41:53 +0300

    Final

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  const form = document.getElementById("create-task-form");
+
+  form.addEventListener("submit",(event) => {
+    event.preventDefault();
+
+    const task = document.getElementById("new-task-description").value;
+
+    buildToDo(task);
+
+    form.reset();
+  });
+
+  function buildToDo(task) {
+    const taskList = document.getElementById("tasks");
+
+    const li = document.createElement("li");
+    li.textContent = task;
+
+    taskList.appendChild(li);
+  }
 });
